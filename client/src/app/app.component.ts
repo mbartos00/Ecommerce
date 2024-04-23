5f47c3470c5ad1fce19337779af131bde41b276d
MBieganGD <mbiegan@griddynamics.com>
2024-04-23 12:52:29 +0200

    feat(prettier,eslint): install and configure eslint with prettier

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,8 +6,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'e-commerce-app';
+  title: string;
+
+  constructor() {
+    this.title = 'e-commerce-app';
+  }
 }
