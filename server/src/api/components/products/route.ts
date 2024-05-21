0162184849e6c1737e4f90dfa2a9bb987cb9472f
akrolikowska <akrolikowska@griddynamics.com>
2024-05-21 10:19:19 +0000

    Fix: 59: categories

--- server/src/api/components/products/route.ts
+++ server/src/api/components/products/route.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import { getAllProducts } from '@src/api/components/products/controllers/all-products';
 import type { Dependecies } from '@src/config/dependencies';
 import { getProductById } from '@src/api/components/products/controllers/single-product';
-import { getAllCategories } from '../categories/controllers/categories';
+import { getAllCategories } from './controllers/categories';
 
 export function productsRouter(deps: Dependecies) {
   const router = Router();
 
   router.get('/', getAllProducts(deps));
+  router.get('/categories', getAllCategories(deps));
   router.get('/:id', getProductById(deps));
-  router.get(`/categories`, getAllCategories(deps));
 
   return router;
 }
