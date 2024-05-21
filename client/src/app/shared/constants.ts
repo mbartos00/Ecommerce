a150725cfeec10e6b08deaf53aca7a054ab6daa1
akrolikowska <akrolikowska@griddynamics.com>
2024-05-21 09:00:32 +0000

    47: Footer

--- client/src/app/shared/constants.ts
+++ client/src/app/shared/constants.ts
@@ -1,1 +1,15 @@
+import { FooterSection } from './footer/footer.component';
+
 export const ACCESS_TOKEN_STORAGE_KEY = 'access_token';
+
+export const FOOTER_SECTIONS: FooterSection[] = [
+  {
+    title: 'Information',
+    links: [
+      { title: 'About Us', url: '/link1' },
+      { title: 'Products', url: '/link2' },
+      { title: 'Our articles', url: '/link3' },
+      { title: 'Your cart', url: '/link4' },
+    ],
+  },
+];
