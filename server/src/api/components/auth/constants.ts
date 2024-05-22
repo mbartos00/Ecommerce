d1bdb6c4c5fa0f788877b19e47c397496fa4c8c9
mbartos <mbartos@griddynamics.com>
2024-05-22 08:03:04 +0000

    8-10: User crud

--- server/src/api/components/auth/constants.ts
+++ server/src/api/components/auth/constants.ts
@@ -1,1 +1,3 @@
 export const REFRESH_TOKEN_COOKIE_NAME = 'jwt_refresh_token';
+
+export const SALT_ROUNDS = 10;
--- server/src/api/components/auth/route.ts
+++ server/src/api/components/auth/route.ts
@@ -1,15 +1,30 @@
-import { Router } from 'express';
-import type { Dependecies } from '@src/config/dependencies';
-import validate from '@src/api/middleware/validate';
-import { loginSchema, registerSchema } from '@src/schemas/auth';
-import { register } from '@src/api/components/auth/controllers/register';
 import { login } from '@src/api/components/auth/controllers/login';
+import { register } from '@src/api/components/auth/controllers/register';
+import { auth } from '@src/api/middleware/auth';
+import validate from '@src/api/middleware/validate';
+import type { Dependecies } from '@src/config/dependencies';
+import {
+  loginSchema,
+  registerSchema,
+  updateUserSchema,
+} from '@src/schemas/auth';
+import { Router } from 'express';
+import { deleteUser } from './controllers/delete-user';
+import { getUserData } from './controllers/get-user-data';
+import { updateUser } from './controllers/update-user';
 
 export function authRouter(deps: Dependecies) {
   const router = Router();
 
+  router.get('/user', auth(deps), getUserData(deps));
   router.post('/register', validate(registerSchema), register(deps));
   router.post('/login', validate(loginSchema), login(deps));
+  router.patch(
+    '/user/update',
+    [auth(deps), validate(updateUserSchema)],
+    updateUser(deps),
+  );
+  router.delete('/user/delete', auth(deps), deleteUser(deps));
 
   return router;
 }
