0065be5e9a389d63f4ad45c1afdcef860f60de32
mbiegan <mbiegan@griddynamics.com>
2024-06-17 11:30:05 +0000

    89 : fix breadcrumbs

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { CartComponent } from './shared/cart/cart.component';
-import { ProductListComponent } from './shared/ui/product-list/product-list.component';
-import { ProductInfoComponent } from './shared/product-info/product-info.component';
 
 export const routes: Routes = [
   {
     path: 'auth',
+    data: { breadcrumb: 'Auth' },
     loadChildren: () => import('./auth/auth.routes').then(m => m.routes),
   },
   {
@@ -13,22 +12,21 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./home/home.component').then(c => c.HomeComponent),
   },
-  { path: 'cart', component: CartComponent },
-  { path: 'shop', component: ProductListComponent },
-  { path: 'product/:id', component: ProductInfoComponent },
+  { path: 'cart', component: CartComponent, data: { breadcrumb: 'Cart' } },
   {
     path: 'shop',
-    loadComponent: () =>
-      import('./shop/shop.component').then(c => c.ShopComponent),
-  },
+    data: { breadcrumb: 'Shop' },
+    loadChildren: () => import('./shop/shop.routes').then(m => m.routes),
   {
     path: 'contact',
+    data: { breadcrumb: 'Contact' },
     loadComponent: () =>
       import('./contact/contact-form.component').then(
         c => c.ContactFormComponent)
   },
   {
     path: 'best',
+    data: { breadcrumb: 'Bestseller' },
     loadComponent: () =>
       import('./shared/bestseller-section/bestseller-section.component').then(
         c => c.BestsellerSectionComponent
--- client/src/app/auth/auth.routes.ts
+++ client/src/app/auth/auth.routes.ts
@@ -3,11 +3,13 @@ import { Route } from '@angular/router';
 export const routes: Route[] = [
   {
     path: 'login',
+    data: { breadcrumb: 'Login' },
     loadComponent: () =>
       import('./login/login.component').then(c => c.LoginComponent),
   },
   {
     path: 'register',
+    data: { breadcrumb: 'Register' },
     loadComponent: () =>
       import('./signup/signup.component').then(c => c.SignupComponent),
   },
