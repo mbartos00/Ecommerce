ab5feb7a3b256d31cd16e3ec71c3098dc1489c31
Maciej Bartos <mbartos@griddynamics.com>
2024-07-10 14:35:05 +0200

    fix: fix products on sale

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,7 +1,7 @@
 import type { PaymentTypes } from '@prisma/client';
 
 export const SIGNALS = ['SIGINT', 'SIGTERM', 'SIGQUIT'] as const;
-export const SALE_PERCENTAGE = 50;
+export const SALE_PERCENTAGE = 40;
 export const DEFAULT_PAGE = '1';
 export const DEFAULT_PER_PAGE = '20';
 export const DEFAULT_UPLOAD_PATH = '/uploads/';
